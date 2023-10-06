f8af140dd518d3b6b0188a5b51f85626e61edbb4
rockreyad <rockreyad@gmail.com>
2023-10-06 20:39:18 +0600

    Initialize project with remix.init script

--- src/database/utils/configs/authConfig.js
+++
@@ -1,5 +0,0 @@
-//secret key as String to encode and decode token
-//- use by verify() and sign() function
-require("crypto").randomBytes(35).toString("hex");
-const jwtSecret = "cat";
-module.exports = jwtSecret;
